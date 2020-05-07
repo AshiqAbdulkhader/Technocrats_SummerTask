7a9cd36c27e0451dfa419e024a3b3bfd7c1900d7
AshiqAbdulkhader <57718729+AshiqAbdulkhader@users.noreply.github.com>
2020-05-07 22:48:43 +0530

    Extra Long Factorials

---
+++ HackerRank/problem1.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    fact=1
+    for i in range(1,n+1):
+        fact=fact*i
+    return fact
+
+if __name__ == '__main__':
+    n = int(input())
+
+    print(extraLongFactorials(n))
